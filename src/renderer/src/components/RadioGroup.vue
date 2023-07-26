3a73eaf486261f7dc7809169bfde641f736c52a2
xiangxiang <1506674357@qq.com>
2023-07-27 00:24:36 +0800

    同步

---
+++ src/renderer/src/components/RadioGroup.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="RadioGroup">
+    <div v-for="(item, i) in options" :key="i" class="flex align-items-center">
+      <RadioButton v-model="value" :value="item.value" />
+      <label class="ml-2">{{ item.name }}</label>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import RadioButton from 'primevue/radiobutton'
+const props = defineProps<{
+  modelValue: string
+  options: { name: string; value: string }[]
+}>()
+const emit = defineEmits(['update:modelValue'])
+const value = computed({
+  get: () => props.modelValue,
+  set: (val) => emit('update:modelValue', val)
+})
+</script>
+
+<style lang="less" scoped>
+#RadioGroup {
+}
+</style>
--- src/renderer/src/pages/Write.vue
+++ src/renderer/src/pages/Write.vue
@@ -1,94 +1,122 @@
 <template>
-  <div id="Note" class="h-full w-full">
-    <span class="p-float-label">
-      <Textarea v-model="contextText" rows="5" cols="30" />
-      <label>原文</label>
-    </span>
-    <div class="flex h-full w-10rem">
-      <div class="item">
-        <div class="header">写作内容</div>
-      </div>
-      <div class="item">
-        <div class="header">操作</div>
-        <div class="btns">
-          <SelectButton
-            v-model="operationSelectedOption"
-            :options="operationOptions"
-            option-label="name"
-            multiple
-            aria-labelledby="multiple"
-          />
+  <div id="Note" class="h-full w-full surface-ground py-5 px-5 flex gap-3 justify-content-between">
+    <!-- <Card
+      :pt="{
+        body: { class: 'h-full' },
+        content: { class: 'contentFull' }
+      }"
+      class="col-2 h-full"
+    >
+      <template #title>参考上下文 </template>
+      <template #content>
+
+      </template>
+    </Card> -->
+    <Card
+      :pt="{
+        body: { class: 'h-full' },
+        content: { class: 'contentActionFull' }
+      }"
+      class="col-6 h-full"
+    >
+      <template #title> 撰写 </template>
+      <template #content>
+        <div class="flex w-full h-full gap-5">
+          <div class="w-5">
+            <div class="header">参考上下文</div>
+            <Textarea
+              v-model="contextText"
+              rows="0"
+              cols="0"
+              :maxlength="20"
+              class="contextText w-full"
+            />
+          </div>
+          <div class="h-full w-7 flex flex-column gap-5">
+            <div class="flex h-auto overflow-y-auto flex-column">
+              <div class="item">
+                <div class="header">写作内容</div>
+                <Textarea v-model="contextText" rows="0" cols="0" class="w-11" />
+              </div>
+              <div class="item">
+                <div class="header">操作</div>
+                <RadioGroup
+                  v-model="operationSelectedOption"
+                  :options="operationOptions"
+                  class="w-full flex flex-wrap justify-content-start gap-3"
+                />
+              </div>
+              <div v-if="operationSelectedOption === 'writing'" class="item">
+                <div class="header">格式</div>
+                <RadioGroup
+                  v-model="formatSelectedOption"
+                  :options="formatOptions"
+                  class="w-full flex flex-wrap justify-content-start gap-3"
+                />
+              </div>
+              <div
+                v-if="['writing', 'continueWriting', 'reply'].includes(operationSelectedOption)"
+                class="item"
+              >
+                <div class="header">语气</div>
+                <RadioGroup
+                  v-model="toneSelectedOption"
+                  :options="toneOptions"
+                  class="w-full flex flex-wrap justify-content-start gap-3"
+                />
+              </div>
+              <div
+                v-if="['writing', 'continueWriting', 'reply'].includes(operationSelectedOption)"
+                class="item"
+              >
+                <div class="header">长度</div>
+                <RadioGroup
+                  v-model="lengthSelectedOption"
+                  :options="lengthOptions"
+                  class="w-full flex flex-wrap justify-content-start gap-3"
+                />
+              </div>
+              <div v-if="operationSelectedOption !== 'translate'" class="item">
+                <div class="header">语言</div>
+                <RadioGroup
+                  v-model="languageSelectedOption"
+                  :options="languageOptions"
+                  class="w-full flex flex-wrap justify-content-start gap-3"
+                />
+              </div>
+            </div>
+            <div class="w-full flex justify-content-center">
+              <Button class="w-9 flex justify-content-center">开始撰写</Button>
+            </div>
+          </div>
         </div>
-      </div>
-      <div v-if="operationSelectedOption === 'writing'" class="item">
-        <div class="header">格式</div>
-        <div class="btns">
-          <SelectButton
-            v-model="formatSelectedOption"
-            :options="formatOptions"
-            option-label="name"
-            multiple
-            aria-labelledby="multiple"
-          />
-        </div>
-      </div>
-      <div
-        v-if="['writing', 'continueWriting', 'reply'].includes(operationSelectedOption)"
-        class="item"
-      >
-        <div class="header">语气</div>
-        <div class="btns">
-          <SelectButton
-            v-model="toneSelectedOption"
-            :options="toneOptions"
-            option-label="name"
-            multiple
-            aria-labelledby="multiple"
-          />
-        </div>
-      </div>
-      <div
-        v-if="['writing', 'continueWriting', 'reply'].includes(operationSelectedOption)"
-        class="item"
-      >
-        <div class="header">长度</div>
-        <div class="btns">
-          <SelectButton
-            v-model="lengthSelectedOption"
-            :options="lengthOptions"
-            option-label="name"
-            multiple
-            aria-labelledby="multiple"
-          />
-        </div>
-      </div>
-      <div v-if="operationSelectedOption !== 'translate'" class="item">
-        <div class="header">语言</div>
-        <div class="btns">
-          <SelectButton
-            v-model="languageSelectedOption"
-            :options="languageOptions"
-            option-label="name"
-            multiple
-            aria-labelledby="multiple"
-          />
-        </div>
-      </div>
-    </div>
-    <span class="p-float-label">
-      <Textarea v-model="contextText" rows="5" cols="30" />
-      <label>原文</label>
-    </span>
+      </template>
+    </Card>
+
+    <Card
+      :pt="{
+        body: { class: 'h-full' },
+        content: { class: 'contentActionFull' }
+      }"
+      class="col-6 h-full"
+    >
+      <template #title>生成结果 </template>
+      <template #content>
+        <Textarea v-model="contextText" rows="0" cols="0" class="h-full w-full" />
+      </template>
+    </Card>
   </div>
 </template>
 
 <script setup lang="ts">
 import Textarea from 'primevue/textarea'
-import SelectButton from 'primevue/selectbutton'
+import Button from 'primevue/button'
+import Card from 'primevue/card'
 import { ref } from 'vue'
-
+import RadioGroup from '@renderer/components/RadioGroup.vue'
 const contextText = ref('')
 const operationSelectedOption = ref('writing')
+
 const formatSelectedOption = ref('auto')
 const toneSelectedOption = ref('professional')
 const lengthSelectedOption = ref('medium')
@@ -137,5 +165,24 @@ const languageOptions = ref([
 
 <style lang="less" scoped>
 #Note {
+  color: #000;
+  :deep(.contentActionFull) {
+    height: calc(100% - 1rem);
+  }
+  .contextText {
+    height: calc(100% - 2rem);
+  }
+  .item {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    margin-bottom: 2rem;
+  }
+  .header {
+    font-size: 1.2rem;
+    font-weight: bold;
+    margin-bottom: 0.5rem;
+  }
 }
 </style>
